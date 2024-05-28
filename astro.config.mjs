7350c02055a6fdd56b5633e5d7735a99c593c63d
Stefen Phelps <stefen@kelp.agency>
2024-05-27 21:43:47 -0400

    bug fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,82 +6,82 @@ import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://bible.stefenphelps.com",
-	trailingSlash: "always",
-	output: "server",
-	compressHTML: true,
-	integrations: [
-		sitemap(),
-		prefetch(),
-		AstroPWA({
-			manifest: {
-				base: "/",
-				scope: "/",
-				orientation: "portrait-primary",
-				name: "The Simple Bible",
-				short_name: "Bible",
-				description: "A well designed simple Bible app powered by Astro.",
-				categories: ["books", "education", "religious"],
-				screenshots: [
-					{
-						src: "home-dark.PNG",
-						sizes: "1179x2556",
-						type: "image/png",
-						platform: "narrow",
-						label: "Homescreen in dark mode"
-					},
-					{
-						src: "home-light.PNG",
-						sizes: "1179x2556",
-						type: "image/png",
-						platform: "narrow",
-						label: "Homescreen in light mode"
-					},
-					{
-						src: "verse-dark.PNG",
-						sizes: "1179x2556",
-						type: "image/png",
-						platform: "narrow",
-						label: "Genesis 1:2 in dark mode"
-					},
-					{
-						src: "verse-light.PNG",
-						sizes: "1179x2556",
-						type: "image/png",
-						platform: "narrow",
-						label: "Genesis 1:2 in light mode"
-					}
-				],
-				start_url: "https://astro-bible.netlify.app",
-				theme_color: "#fff7ed",
-				background_color: "#fff7ed",
-				display: "standalone",
-				includeAssets: ["*.{png,ico,svg,jpg,xml}"],
-				icons: [
-					{
-						src: "/pwa-192x192.png",
-						sizes: "192x192",
-						type: "image/png"
-					},
-					{
-						src: "/safari-pinned-tab.svg",
-						sizes: "512x512",
-						type: "image/png",
-						purpose: "maskable"
-					},
-					{
-						src: "/pwa-512x512.png",
-						sizes: "512x512",
-						type: "image/png",
-						purpose: "any"
-					}
-				]
-			},
-			workbox: {
-				navigateFallback: "/404",
-				globPatterns: ["**/*.{css,js,html,svg,png,ico,txt}"]
-			}
-		})
-	],
-	adapter: netlify()
+  site: "https://bible.stefenphelps.com",
+  trailingSlash: "always",
+  output: "server",
+  compressHTML: true,
+  integrations: [
+    sitemap(),
+    prefetch(),
+    AstroPWA({
+      manifest: {
+        base: "/",
+        scope: "/",
+        orientation: "portrait-primary",
+        name: "The Simple Bible",
+        short_name: "Bible",
+        description: "A well designed simple Bible app powered by Astro.",
+        categories: ["books", "education", "religious"],
+        screenshots: [
+          {
+            src: "home-dark.PNG",
+            sizes: "1179x2556",
+            type: "image/png",
+            platform: "narrow",
+            label: "Homescreen in dark mode",
+          },
+          {
+            src: "home-light.PNG",
+            sizes: "1179x2556",
+            type: "image/png",
+            platform: "narrow",
+            label: "Homescreen in light mode",
+          },
+          {
+            src: "verse-dark.PNG",
+            sizes: "1179x2556",
+            type: "image/png",
+            platform: "narrow",
+            label: "Genesis 1:2 in dark mode",
+          },
+          {
+            src: "verse-light.PNG",
+            sizes: "1179x2556",
+            type: "image/png",
+            platform: "narrow",
+            label: "Genesis 1:2 in light mode",
+          },
+        ],
+        start_url: "https://bible.stefenphelps.com",
+        theme_color: "#fff7ed",
+        background_color: "#fff7ed",
+        display: "standalone",
+        includeAssets: ["*.{png,ico,svg,jpg,xml}"],
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/safari-pinned-tab.svg",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+        ],
+      },
+      workbox: {
+        navigateFallback: "/404",
+        globPatterns: ["**/*.{css,js,html,svg,png,ico,txt}"],
+      },
+    }),
+  ],
+  adapter: netlify(),
 });
--- src/pages/api/ask.json.js
+++ src/pages/api/ask.json.js
@@ -1,48 +1,67 @@
-export async function post({ request }) {
-	const body = await request.json();
+export async function POST({ request }) {
+  try {
+    const body = await request.json();
 
-	const data = {
-		model: "gpt-3.5-turbo",
-		messages: [
-			{
-				role: "system",
-				content:
-					"You are a pastor for a protestant church. All your responses should try to include Bible references."
-			},
-			{ role: "user", content: `${body.question}` }
-		],
-		temperature: 0.7,
-		max_tokens: 700
-	};
+    const data = {
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "system",
+          content:
+            "You are a pastor for a protestant church. All your responses should try to include Bible references.",
+        },
+        { role: "user", content: `${body.question}` },
+      ],
+      temperature: 0.7,
+      max_tokens: 700,
+    };
 
-	const chatGpt = await fetch("https://api.openai.com/v1/chat/completions", {
-		headers: {
-			Authorization: `Bearer ${import.meta.env.OPENAI_KEY}`,
-			"Content-Type": "application/json"
-		},
-		method: "POST",
-		body: JSON.stringify(data)
-	});
+    const chatGpt = await fetch("https://api.openai.com/v1/chat/completions", {
+      headers: {
+        Authorization: `Bearer ${import.meta.env.OPENAI_KEY}`,
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify(data),
+    });
 
-	const response = await chatGpt.json();
-	const text = response.choices[0].message.content;
+    const response = await chatGpt.json();
+    console.log("OpenAI API response:", response);
 
-	// log question
-	console.log(body.question);
-	// log answer
-	console.log(text);
+    if (!response.choices || response.choices.length === 0) {
+      throw new Error("No choices returned from OpenAI API");
+    }
 
-	return new Response(
-		JSON.stringify({
-			message: text
-		}),
-		{
-			status: 200,
-			headers: {
-				"Content-Type": "application/json"
-			}
-		}
-	);
+    const text = response.choices[0].message.content;
 
-	return new Response(null, { status: 400 });
+    // log question
+    console.log(body.question);
+    // log answer
+    console.log(text);
+
+    return new Response(
+      JSON.stringify({
+        message: text,
+      }),
+      {
+        status: 200,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  } catch (error) {
+    console.error("Error:", error);
+    return new Response(
+      JSON.stringify({
+        error: error.message,
+      }),
+      {
+        status: 500,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
 }
